dd31a6b48ad7af7284d5bbff9dd40aed289c3dfc
smoothdvd <madfxgao@gmail.com>
2022-02-23 08:50:07 +0800

    update: remove default queue

--- server/config/index.js
+++ server/config/index.js
@@ -5,15 +5,7 @@ module.exports = {
     settings: {
       debug: false,
     },
-    queues: {
-      default: {
-        redis: {
-          host: '127.0.0.1',
-          port: 6379,
-          db: 0,
-        },
-      }
-    }
+    queues: {}
   },
   validator(/* config */) { },
 };
