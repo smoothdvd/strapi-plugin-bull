598cac699823fc9e49bc9b5c502326af0e696189
smoothdvd <madfxgao@gmail.com>
2023-02-16 17:17:58 +0800

    update: use default as global option

--- server/services/queue.js
+++ server/services/queue.js
@@ -1,3 +1,5 @@
+/* eslint no-param-reassign: ["error", { "props": true, "ignorePropertyModificationsFor": ["strapi"] }] */
+
 'use strict';
 
 const Bull = require('bull');
@@ -6,28 +8,33 @@ const debug = require('debug')('strapi:strapi-plugin-bull');
 
 module.exports = ({ strapi }) => ({
   buildAll(config) {
-    const coreConfig = config
+    // default redis config
+    const {
+      default: { redis = {} },
+    } = config;
 
-    Object.keys(coreConfig.queues).forEach((name) => {
+    Object.keys(config.queues).forEach((name) => {
       debug(`${chalk.yellow('Building')} ${name} queue`);
-      const nameConfig = coreConfig.queues[name];
+      const queueConfig = config.queues[name];
       try {
-        strapi.bull.queues[name] = new Bull(name, { redis: nameConfig.redis });
+        strapi.bull.queues[name] = new Bull(name, { redis: { ...redis, ...queueConfig.redis } });
         debug(`${chalk.green('Built')} ${name} queue`);
       } catch (e) {
         debug(`${chalk.red('Failed to build')} ${name} queue`);
       }
       try {
-        if (nameConfig.process) {
+        if (queueConfig.process) {
           strapi.bull.queues[name].process(
-            nameConfig.process.name,
-            nameConfig.process.concurrency,
-            nameConfig.process.processor({ strapi }).process
+            queueConfig.process.name,
+            queueConfig.process.concurrency,
+            queueConfig.process.processor({ strapi }).process
           );
-          debug(`${chalk.green('Created')} ${name} queue ${nameConfig.process.name} processor`);
+          debug(`${chalk.green('Created')} ${name} queue ${queueConfig.process.name} processor`);
         }
       } catch (e) {
-        debug(`${chalk.red('Failed to create')} ${name} queue ${nameConfig.process.name} processor`);
+        debug(
+          `${chalk.red('Failed to create')} ${name} queue ${queueConfig.process.name} processor`
+        );
       }
     });
   },
